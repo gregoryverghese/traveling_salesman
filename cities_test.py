35d261caecf8bbbbfd6c41fdf7aaa0de92d1e118
gregory verghese <gvergh01@dcs.bbk.ac.uk>
2018-03-24 12:54:46 +0000

    added pytest fixture to return road_map for testing functions. Added the test_swap_adjacent_cities function, which is currently faiing to due to only stub method

--- cities_test.py
+++ cities_test.py
@@ -2,23 +2,42 @@ import pytest
 from cities import *
 from earth_distance import distance
 
+@pytest.fixture()
+def road_map():
 
-def test_compute_total_distance():
-
-    road_map = [('Alabama', 'Montgomery', 32.361538, -86.279118),
-                    ('Alaska', 'Juneau', 58.301935, -134.41974)]
-
-    total_distance = compute_total_distance((road_map))
-
-    assert total_distance ==  pytest.approx(2850, abs=50)
-
-    road_map2 = [('Alabama', 'Montgomery', 32.361538, -86.279118),
+    road_map_list = [('Alabama', 'Montgomery', 32.361538, -86.279118),
                 ('Alaska', 'Juneau', 58.301935, -134.41974),
                 ('Arizona', 'Phoenix', 33.448457, -112.073844)]
 
+    return road_map_list
 
-    total_distance2 = compute_total_distance(road_map2)
 
-    print(total_distance2)
+def test_compute_total_distance(road_map):
+
+    print(road_map)
+
+    total_distance2 = compute_total_distance(road_map)
 
     assert total_distance2 == pytest.approx(6356, abs=30)
+
+    road_map1 = [('Alabama', 'Montgomery', 32.361538, -86.279118),
+                ('Alaska', 'Juneau', 58.301935, -134.41974)]
+
+    total_distance = compute_total_distance((road_map1))
+
+    assert total_distance ==  pytest.approx(2970, abs=50)
+
+
+def test_swap_adjacent_cities(road_map):
+
+    adjusted_road_map = [('Alabama', 'Montgomery', 32.361538, -86.279118),
+                        ('Arizona', 'Phoenix', 33.448457, -112.073844),
+                        ('Alaska', 'Juneau', 58.301935, -134.41974)]
+
+    new_road_map_tuple = swap_adjacent_cities(road_map, index=1)
+
+    new_road_map = new_road_map_tuple[0]
+    new_distance = new_road_map_tuple[1]
+
+    assert new_road_map == adjusted_road_map
+    assert new_distance == pytest.approx(2000, abs=50)
